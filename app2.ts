adb236485c3f8862fcb2d9f9aa0cbaed4c85e1f9
kairoLamarca <lamarca.kairo@gmail.com>
2017-11-07 13:50:27 -0200

    import, export e class carro, concessionaria e pessoa
    implementação das calsses carro, concessionaria e pessoa separadamente. Utilização do export e import para usar uma classe dentro da outra

--- app2.ts
+++ app2.ts
@@ -1,100 +1,24 @@
-class Carro {
-    private modelo: string;
-    private numeroDePortas: number;
-    private velocidade: number = 0;
-
-    constructor(modelo: string, numeroDePortas: number) {
-        this.modelo = modelo;
-        this.numeroDePortas = numeroDePortas;
-    }
-
-    public acelerar(): void {
-        this.velocidade = this.velocidade + 10;
-    }
-
-    public parar(): void {
-        this.velocidade = 0;
-    }
-
-    public velocidadeAtual(): number {
-        return this.velocidade;
-    }
-}
-
-// let carroA = new Carro('Veloster', 3);
-// console.log(carroA);
-// carroA.acelerar();
-// console.log(carroA);
-// carroA.acelerar();
-// carroA.acelerar();
-// console.log(carroA);
-
-class Concessionaria {
-    private endereco: string;
-    private listaDeCarros: Array<Carro>;
-
-    constructor(endereco: string, listaDeCarros: Array<Carro>) {
-        this.endereco = endereco;
-        this.listaDeCarros = listaDeCarros;
-    }
-
-    public fornecerEndereco(): string {
-        return this.endereco;
-    }
-
-    public mostrarListaDeCarros(): Array<Carro> {
-        return this.listaDeCarros;
-    }
-}
-
-// let concessionaria = new Concessionaria('Av Paulista');
-// console.log(concessionaria);
-
-class Pessoa {
-    private nome: string;
-    private carroPreferido: string;
-    private carro: Carro;
-
-    constructor(nome: string, carroPreferido: string) {
-        this.nome = nome;
-        this.carroPreferido = carroPreferido;
-    }
-
-    public dizerNome(): string {
-        return this.nome;
-    }
-
-    public dizerCarroPreferido(): string {
-        return this.carroPreferido;
-    }
-
-    public comprarCarro(carro: Carro): void {
-        this.carro = carro;
-    }
-
-    public dizerCarroQueTem(): Carro {
-        return this.carro;
-    }
-}
+import Carro from './Carro';//import { Carro } from './Carro';
+import Pessoa from './Pessoa';
+import Concessionaria from './Concessionaria';
 
 let pessoa = new Pessoa('José', 'Veloster');
 //console.log(pessoa.dizerCarroPreferido());
 
-/* Criar Carros */
+/* --- Criar Carros --- */
 let carroA = new Carro('DogeJourney', 4);
 let carroB = new Carro('Veloster', 3);
 let carroC = new Carro('Cerato', 4);
 
-/* Montar a lista de carros da concessionaria */
-//let listaDeCarros: Array<Carro> = [carroA, carroB, carroC];
+/* --- Montar a lista de carros da concessionaria --- */
 let listaDeCarros: Carro[] = [carroA, carroB, carroC];
 
 let concessionaria = new Concessionaria('Av paulista', listaDeCarros);
 
-/* Exibir a lista de carros */
+/* --- Exibir a lista de carros --- */
 //console.log(concessionaria.mostrarListaDeCarros());
 
-/* comprar o carro */
+/* --- comprar o carro --- */
 let cliente = new Pessoa('João', 'Veloster');
 //console.log(cliente.dizerCarroPreferido());
 
--- dist/app2.js
+++ dist/app2.js
@@ -1,76 +1,21 @@
 "use strict";
-var Carro = /** @class */ (function () {
-    function Carro(modelo, numeroDePortas) {
-        this.velocidade = 0;
-        this.modelo = modelo;
-        this.numeroDePortas = numeroDePortas;
-    }
-    Carro.prototype.acelerar = function () {
-        this.velocidade = this.velocidade + 10;
-    };
-    Carro.prototype.parar = function () {
-        this.velocidade = 0;
-    };
-    Carro.prototype.velocidadeAtual = function () {
-        return this.velocidade;
-    };
-    return Carro;
-}());
-// let carroA = new Carro('Veloster', 3);
-// console.log(carroA);
-// carroA.acelerar();
-// console.log(carroA);
-// carroA.acelerar();
-// carroA.acelerar();
-// console.log(carroA);
-var Concessionaria = /** @class */ (function () {
-    function Concessionaria(endereco, listaDeCarros) {
-        this.endereco = endereco;
-        this.listaDeCarros = listaDeCarros;
-    }
-    Concessionaria.prototype.fornecerEndereco = function () {
-        return this.endereco;
-    };
-    Concessionaria.prototype.mostrarListaDeCarros = function () {
-        return this.listaDeCarros;
-    };
-    return Concessionaria;
-}());
-// let concessionaria = new Concessionaria('Av Paulista');
-// console.log(concessionaria);
-var Pessoa = /** @class */ (function () {
-    function Pessoa(nome, carroPreferido) {
-        this.nome = nome;
-        this.carroPreferido = carroPreferido;
-    }
-    Pessoa.prototype.dizerNome = function () {
-        return this.nome;
-    };
-    Pessoa.prototype.dizerCarroPreferido = function () {
-        return this.carroPreferido;
-    };
-    Pessoa.prototype.comprarCarro = function (carro) {
-        this.carro = carro;
-    };
-    Pessoa.prototype.dizerCarroQueTem = function () {
-        return this.carro;
-    };
-    return Pessoa;
-}());
-var pessoa = new Pessoa('José', 'Veloster');
+Object.defineProperty(exports, "__esModule", { value: true });
+var Carro_1 = require("./Carro"); //import { Carro } from './Carro';
+var Pessoa_1 = require("./Pessoa");
+var Concessionaria_1 = require("./Concessionaria");
+var pessoa = new Pessoa_1.default('José', 'Veloster');
 //console.log(pessoa.dizerCarroPreferido());
-/* Criar Carros */
-var carroA = new Carro('DogeJourney', 4);
-var carroB = new Carro('Veloster', 3);
-var carroC = new Carro('Cerato', 4);
-/* Montar a lista de carros da concessionaria */
-//let listaDeCarros: Array<Carro> = [carroA, carroB, carroC];
+/* --- Criar Carros --- */
+var carroA = new Carro_1.default('DogeJourney', 4);
+var carroB = new Carro_1.default('Veloster', 3);
+var carroC = new Carro_1.default('Cerato', 4);
+/* --- Montar a lista de carros da concessionaria --- */
 var listaDeCarros = [carroA, carroB, carroC];
-var concessionaria = new Concessionaria('Av paulista', listaDeCarros);
-/* Exibir a lista de carros */
+var concessionaria = new Concessionaria_1.default('Av paulista', listaDeCarros);
+/* --- Exibir a lista de carros --- */
 //console.log(concessionaria.mostrarListaDeCarros());
-/* comprar o carro */
-var cliente = new Pessoa('João', 'Veloster');
+/* --- comprar o carro --- */
+var cliente = new Pessoa_1.default('João', 'Veloster');
 //console.log(cliente.dizerCarroPreferido());
 concessionaria.mostrarListaDeCarros().map(function (carro) {
     //console.log(carro);
