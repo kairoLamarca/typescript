5ad180177280425c8aed4ec06a07bf164a6de7cb
kairoLamarca <lamarca.kairo@gmail.com>
2017-11-08 13:21:55 -0200

    Dao generics
    implementação da classe generica Dao utilizando o T(type)

--- app4.ts
+++ app4.ts
@@ -1,15 +1,24 @@
-import { ConcessionariaDao } from './ConcessionariaDao';
+import { Dao } from './Dao';
+
+//import { ConcessionariaDao } from './ConcessionariaDao';
 import Concessionaria from './Concessionaria';
 
-import { PessoaDao } from './PessoaDao';
+//import { PessoaDao } from './PessoaDao';
 import Pessoa from './Pessoa';
 
-let dao: ConcessionariaDao = new ConcessionariaDao();
+//let dao: ConcessionariaDao = new ConcessionariaDao();
 let concessionaria = new Concessionaria('', []);
 
-dao.inserir(concessionaria);
+//dao.inserir(concessionaria);
 
-let dao2: PessoaDao = new PessoaDao();
+//let dao2: PessoaDao = new PessoaDao();
 let pessoa: Pessoa = new Pessoa('', '');
 
-dao2.atualizar(pessoa);
+//dao2.atualizar(pessoa);
+
+let dao3: Dao<Concessionaria> = new Dao<Concessionaria>();
+let dao4: Dao<Pessoa> = new Dao<Pessoa>();
+
+dao3.inserir(concessionaria);
+
+dao4.remover(5);
